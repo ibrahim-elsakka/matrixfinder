464c003a41d0653bd54a6c60a65fc427c182533e
coltonon <cnpearson42@gmail.com>
2018-06-17 16:45:52 -0500

    offsets update

--- matrixfinder/offsets.h
+++ matrixfinder/offsets.h
@@ -4,7 +4,9 @@
 
 // Created with ReClass.NET by KN4CK3R
 
-#define OFFSET_GAMERENDERER 0x144520FE0
+#define OFFSET_GAMERENDERER 0x144523de0
+
+// Created with ReClass.NET by KN4CK3R
 
 class RenderView
 {
@@ -30,3 +32,4 @@ public:
 	DWORD_PTR gameRenderer; //0x0000
 	char pad_0008[56]; //0x0008
 }; //Size: 0x0040
+
